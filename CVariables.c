ef2d463f64770e3b0691d4b2fa0d5c5bb56d941e
OmoshJatelo <calvinceomondi913@gmail.com>
2024-10-05 20:24:29 +0300

    Add files via upload
    today i learned how to assign values to variables

---
+++ CVariables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    char name[] = "Jatelo";
+    int score = 99;
+    float gpa = 3.98;
+    char grade = 'A';
+    printf("\nHello %s\n",name);
+    printf("your score in the previous test is %f ",gpa);
+    printf("and your final grade in the test is %c\n",grade);
+    printf("Keep the tide high, its just a matter of consistence and persistence\n");
+
+
+    return 0;
+}
