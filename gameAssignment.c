ed8f550f4c96d26217e89aa2b9dea2a84b210d4a
OmoshJatelo <calvinceomondi913@gmail.com>
2024-10-18 07:28:33 +0300

    added the game assignment file created with C language

---
+++ gameAssignment.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    srand(time(0));   //srand(time(0))
+    int secreteNum= rand()%10 +9;
+    int rounds = 0;
+    int srounds= 0;      
+    int totalScore =0;
+    int userInput; //lucky number
+    int remainder;
+    printf("\nenter the number of rounds you want to play:\n ");
+    scanf("%d", &srounds);
+    rounds+=srounds;
+
+
+    while (rounds>=1){
+        printf("\nenter your lucky number: \n");
+        scanf("%d", &userInput);
+        remainder= userInput%secreteNum;
+
+        if(remainder==0){
+            printf("Good attempt, this is a draw you have earned one point. keep trying\n");
+            totalScore+=1;
+            
+        }
+        else if(remainder%2==0){
+            printf("Bingo! You smashed it. you have received additional three points\n\n");
+            totalScore+=3;
+
+        }
+        else{
+            printf("\n\nOops! Seems like luck was not on you side this time. you have lost three point\n keep trying, Legends never die\n\n");
+            totalScore-=3;
+
+        }
+
+        rounds--;
+              
+    }
+
+    if(totalScore>0){
+        printf("Congratulations! You are the real goat.\n Keep playing to establish a legacy that will never die\n Good luck in the next round\n\n");
+
+    }
+    else{
+        printf("Oops! the pot broke when you were just at the door\n Just keep trying\n\n");
+    }
+
+
+        
+    return 0;
+}
